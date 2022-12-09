0bc4d9dedccef5f0a2bec41bf4601b1582cb69b8
Lukas Wiese <eseiwsakul@gmail.com>
2022-12-09 22:45:38 +0100

    Added statusbar at bottom of view

--- src/View.py
+++ src/View.py
@@ -21,8 +21,9 @@ class View:
 		self.menuFile    = tk.Menu(self.menu)
 		self.menuAccount = tk.Menu(self.menu)
 
+		self.statusbar = tk.Frame(self.root) #StatusBar(self.root)
 		self.mainframe = AccountListView(self.root, tracker)
-		self.mainframe.pack(fill='x')
+		self.mainframe.grid(row=0, column=0, sticky='nswe')
 
 		self._setup()
 
@@ -34,14 +35,24 @@ class View:
 	def open_account_list_view(self):
 		self.mainframe.destroy()
 		self.mainframe = AccountListView(self.root, tracker)
-		self.mainframe.pack(fill='x')
-
+		self.mainframe.grid(row=0, column=0, sticky='nswe')
 
 	def open_account_view(self, account):
 		self.mainframe.destroy()
 		logging.info(f"Open account '{account.username}'")
 		self.mainframe = AccountView(self.root, account, self.tracker)
-		self.mainframe.pack(fill='x')
+		self.mainframe.grid(row=0, column=0, sticky='nswe')
+
+	def msg(self, text, clear_after=0):
+		self.msglbl = LeftLabel(self.statusbar, text=text, font='Arial 10',
+				fg='grey', bg='#303030')
+		self.msglbl.grid(row=0, column=0, sticky='nw')
+
+		if clear_after > 0:
+			self.msglbl.after(clear_after*1000, self.clear)
+
+	def clearmsg(self):
+		self.msglbl.destroy()
 
 
 	def _setup(self):
@@ -50,6 +61,18 @@ class View:
 		self.menu.add_cascade(label='Account', menu=self.menuAccount)
 		self.menuFile.add_command(label="Exit", command=self.root.destroy)
 		self.root.grid_columnconfigure(0, weight=1)
+		self.root.grid_rowconfigure(0, weight=1)
+
+		self.statusbar.configure(bg='#303030', height=30,
+			highlightbackground="#454545", highlightthickness=1)
+		self.statusbar.grid(row=1, column=0, sticky='nswe')
+		#self.statusbar.msg('Hello World', 3)
+
+
+
+class StatusBar(tk.Frame):
+	def __init__(self, parent):
+		super().__init__(parent)
 
 
 
