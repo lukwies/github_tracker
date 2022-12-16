affcdaa1bf0f03a553bc7b040ba6a080e1645703
Lukas Wiese <eseiwsakul@gmail.com>
2022-12-16 21:37:23 +0100

    Menu functions are changing on different views now

--- src/AccountListView.py
+++ src/AccountListView.py
@@ -5,7 +5,7 @@ from GithubAccount import *
 
 class AccountListView(tk.Frame):
 
-	def __init__(self, parent, tracker):
+	def __init__(self, parent, tracker, view):
 		'''
 		Account list view.
 
@@ -16,14 +16,32 @@ class AccountListView(tk.Frame):
 		super().__init__(parent)
 
 		self.tracker = tracker
-		self.__setup()
+		self.view = view
+		self.max_row = 0
+		self.setup()
 
+	def add(self, account):
+		'''
+		Add account to List View.
+		'''
+		AccountListFrame(self, account, self.tracker).grid(
+				row=self.max_row, column=0, sticky='nswe')
+		self.max_row += 1
 
-	def __setup(self):
+	def setup(self):
+                # Setup menu
+		self.view.menu.entryconfig('Account', state='normal')
+		self.view.menuView.entryconfig('Sort by last commit', state='normal',
+			command=lambda: self.tracker.sort_accounts('commit'))
+		self.view.menuView.entryconfig('Sort by name', state='normal',
+			command=lambda: self.tracker.sort_accounts('name', False))
+		self.view.menuView.entryconfig('Sort by number of repos', state='normal',
+			command=lambda: self.tracker.sort_accounts('repos'))
 
 		self.grid_columnconfigure(0, weight=1)
 
-		for i, acc in enumerate(self.tracker.accounts):
+		for acc in self.tracker.accounts:
 			AccountListFrame(self, acc, self.tracker).grid(
-					row=i, column=0, sticky='nswe')
+					row=self.max_row, column=0, sticky='nswe')
+			self.max_row += 1
 
