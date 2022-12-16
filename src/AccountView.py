829d1c9b7404560f56f94af78b9c9093a58c2a1f
Lukas Wiese <eseiwsakul@gmail.com>
2022-12-16 21:39:15 +0100

    Sorting repositories is possible now (by commit date or name)

--- src/AccountView.py
+++ src/AccountView.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 from PIL import Image,ImageTk
-from widgets import *
-import textwrap
 from datetime import datetime
+import textwrap
 
+from widgets import *
+from util import *
+
+font_col1 = '#090909'
+font_col2 = '#151515'
+font_col3 = '#454545'
 
 class AccountView(tk.Frame):
 	'''
@@ -23,9 +28,18 @@ class AccountView(tk.Frame):
 		self.frBack  = tk.Frame(self, bg='#303030')
 		self.btnBack = tk.Button(self.frBack, text='Back', bg='#808080',
 					cursor='hand2', command=tracker.view.open_account_list_view)
-		self.__setup()
+		self.setup()
 
-	def __setup(self):
+	def setup(self):
+		# Setup menu
+		v = self.tracker.view
+		v.menu.entryconfig('Account', state='disabled')
+		v.menuView.entryconfig('Sort by last commit', state='normal',
+			command=lambda: self.sort('commit', True))
+		v.menuView.entryconfig('Sort by name', state='normal',
+			command=lambda: self.sort('name', False))
+		v.menuView.entryconfig('Sort by number of repos', state='disabled')
+
 		self.grid_columnconfigure(1, weight=1)
 		self.grid_rowconfigure(0, weight=1)
 
@@ -34,6 +48,17 @@ class AccountView(tk.Frame):
 
 		self.btnBack.grid(row=0, column=0, sticky='nswe')
 		self.frBack.grid(row=2, column=0, columnspan=2, sticky='nswe')
+
+
+	def sort(self, by='commit', reverse=False):
+		'''
+		Sort repositories and refresh screen
+		'''
+		self.account.sort_repos(by, reverse)
+		self.repoFrame.destroy()
+		self.repoFrame = RepoList(self, self.account, self.tracker)
+		self.repoFrame.grid(row=0, column=1, sticky='nswe')
+
 
 
 
@@ -49,35 +74,54 @@ class Sidebar(tk.Frame):
 		self.__setup()
 
 	def __setup(self):
+		# Avatar image (can be clicked to open avatar view)
 		self.img = ImageTk.PhotoImage(Image.open(self.account.avatar_file).resize((180,180)))
-		tk.Label(self, image=self.img).grid(row=0, column=0, columnspan=2, sticky='nw', padx=3, pady=3)
+		ilbl     = tk.Label(self, image=self.img, cursor='hand2', bg='#424242')
+		ilbl.bind('<Button-1>', lambda ev: self.tracker.view.open_avatar_view(self.account))
+		ilbl.grid(row=0, column=0, columnspan=2, sticky='nswe', padx=2, pady=2)
 
+		# Account name (can be clicked to open github account with default browser)
 		URLLabel(self, self.account.username, 'https://github.com/' + self.account.username,
-				font='Arial 14 bold').grid(row=1, column=0, columnspan=2, sticky='swe', padx=3)
+			font='Arial 15 bold', fg=font_col1).grid(
+				row=1, column=0, columnspan=2, sticky='swe', padx=3, pady=5)
 		rowi=2
 
+		# Real username (can be empty)
 		if self.account.realname != '':
-			make_label(self, self.account.realname, 'Arial 9', 'grey').grid(
-					row=rowi, column=0, columnspan=2, sticky='nwe', padx=3)
+			LeftLabel(self, text=self.account.realname,
+				font='Arial 10 bold', fg=font_col1).grid(
+				row=rowi, column=0, columnspan=2, sticky='nwe', padx=3)
 			rowi += 1
 
+		# Account user location (can be empty)
 		if self.account.location != '':
-			make_label(self, 'Location: ' + self.account.location, 'Arial 10', 'grey').grid(
-					row=rowi, column=0, columnspan=2, sticky='nswe', padx=3)
+			LeftLabel(self, text='From: ' + self.account.location,
+				font='Arial 10', fg=font_col2).grid(
+				row=rowi, column=0, columnspan=2, sticky='nswe', padx=3)
 			rowi += 1
 
+		# Account description (can be empty)
 		if self.account.userinfo != '':
 			info = '\n'.join(textwrap.wrap(self.account.userinfo, 30))
-			make_label(self, info, 'Arial 9').grid(
-					row=rowi, column=0, columnspan=2, sticky='nwe', padx=3, pady=5)
+			LeftLabel(self, text=info, font='Arial 9', fg=font_col2).grid(
+				row=rowi, column=0, columnspan=2, sticky='nwe', padx=3, pady=5)
 			rowi += 1
 
-		s = f'Followers: {self.account.followers}\nFollowing: {self.account.following}'
-		LeftLabel(self, text=s).grid(row=rowi, column=0, columnspan=2, sticky='nswe', padx=3)
+		# Followers (can be clicked to open github follower page in browser)
+		URLLabel(self, f'Followers:  {self.account.followers}',
+			f'https://github.com/{self.account.username}?tab=followers',
+			font='Arial 9', fg=font_col2).grid(
+			row=rowi, column=0, columnspan=2, sticky='nswe', padx=3)
 		rowi += 1
 
-		self.grid_rowconfigure(6, weight=1)
-		self.configure(bg='#535353')
+		# Following (can be clicked to open github following page in browser)
+		URLLabel(self, f'Following:  {self.account.following}',
+			f'https://github.com/{self.account.username}?tab=following',
+			font='Arial 9', fg=font_col2).grid(
+				row=rowi, column=0, columnspan=2, sticky='nswe', padx=3)
+
+		self.grid_rowconfigure(10, weight=1)
+		self.configure(bg='#424242')
 
 
 class RepoList(ScrollFrame):
@@ -89,16 +133,17 @@ class RepoList(ScrollFrame):
 		super().__init__(parent)
 		self.account = account
 		self.tracker = tracker
-		self.__setup()
+		self.setup()
 
-	def __setup(self):
+	def setup(self):
 		if len(self.account.repos) > 0:
 			for i, repo in enumerate(self.account.repos):
 				RepoListItem(self.scroll_frame, repo, self.tracker).grid(
 					row=i, column=0, sticky='we')
 		else:
 			tk.Label(self.scroll_frame, text='No repositories :-(',
-				font='Arial 12 bold').grid(row=0, column=0, sticky='nswe')
+				font='Arial 16 bold').grid(row=0, column=0, sticky='nswe')
+
 
 '''
 +---------------+---------------+---------------+
@@ -126,42 +171,39 @@ class RepoListItem(tk.Frame):
 				highlightbackground="#323232", highlightthickness=1)
 		self.grid_columnconfigure(1, weight=1)
 
+		# Repo name (can be clicked to open github repo in browser)
 		URLLabel(self, self.repo['name'], 'https://github.com' + self.repo['url'],
-				font='Arial 12 bold').grid(row=0, column=0, columnspan=2, sticky='nswe')
-		make_label(self, self.repo['language'], 'Arial 8', '#989898').grid(
-				row=0, column=2, sticky='nswe')
+				font='Arial 13 bold', fg=font_col1).grid(
+				row=0, column=0, columnspan=2, sticky='nswe')
 
-		make_label(self, 'Last commit:', 'Arial 8 bold').grid(
-			row=1, column=0, sticky='nswe')
+		# Repo language (Can be empty)
+		LeftLabel(self, text=self.repo['language'], font='Arial 8',
+			fg=font_col3).grid(row=0, column=2, sticky='nswe', ipadx=3)
 
-		# If repository was commited today, print red date
-		d = self.repo['last_update']
-		if d.date() == datetime.today().date():
-			LeftLabel(self, text=d.strftime('%c'), font='Arial 8 bold',
-				fg='#E00000').grid(
+		# Date of last commit
+		# If repository was commited today, print date in red color.
+		d  = self.repo['last_commit']
+		fg = '#E00000' if date_is_today(d) else font_col2
+		ds = get_time_exceeded(d)
+
+		LeftLabel(self, text='Last commit:', font='Arial 9 bold',
+			fg=font_col2).grid(row=1, column=0, sticky='nswe')
+		LeftLabel(self, text=ds, font='Arial 8', fg=fg).grid(
 				row=1, column=1, columnspan=2, sticky='nswe')
-		else:
-			LeftLabel(self, text=d.strftime('%c'), font='Arial 8').grid(
-				row=1, column=1, columnspan=2, sticky='nswe')
-
 		rowi = 2
 
+		# If repo is a fork, show link of the original repository
 		if self.repo['type'] == 'fork':
-			make_label(self, 'Forked from:', 'Arial 8 bold').grid(
-				row=rowi, column=0, sticky='nswe')
+			LeftLabel(self, text='Forked from:', font='Arial 8 bold',
+				fg=font_col2).grid(row=rowi, column=0, sticky='nswe')
 
 			URLLabel(self, self.repo['forked_from'], 'https://github.com' + self.repo['forked_from'],
 				font='Arial 8').grid(row=rowi, column=1, columnspan=2, sticky='nswe')
 			rowi += 1
 
+		# Account description text (Can be empty)
 		if self.repo['description'] != '':
-			make_label(self, self.repo['description'], 'Arial 8').grid(
+			info = '\n'.join(textwrap.wrap(self.repo['description'], 120))
+			LeftLabel(self, text=info, font='Arial 8', fg=font_col2).grid(
 				row=rowi, column=0, columnspan=3, sticky='nswe')
 			rowi += 1
-
-
-
-## TODO REMOVE
-def make_label(parent, text, font, fg='black'):
-	return tk.Label(parent, text=text, font=font, fg=fg, justify='left', anchor='w')
-
